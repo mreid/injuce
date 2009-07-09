cb923df888834d026e9b9678700eaf3a0d36ba13
mreid <mark@reid.name>
2009-07-09 22:24:51 +1000

    Work in progress

--- sgd.clj
+++ sgd.clj
@@ -3,7 +3,35 @@
 ;; AUTHOR: Mark Reid <mark@reid.name>
 ;; CREATED: 2009-07-08
 
+; Basic online convex optimisation (OCO) game is as follows:
+;	1.	Repeat
+;	1.1		Player makes an play p_t
+;	1.2		World gives player response r_t
+;	1.3		Player incurs some cost l(p_t,r_t)
+
 (ns sgd)
+
+; Convert a file to a lazy sequence of lines.
+; (-> "sgd.clj" FileReader. BufferedReader. line-seq)
+
+; World will wrap this lazy sequence and use it for its responses.
+(defn response
+	"Returns the next example from a sequence, ignoring the input"
+	[]
+	)
+
+(defn oco-step
+	"Performs a single step of the OCO game and returns the loss incured"
+	[]
+	(let [pt (play player)
+		  rt (respond world pt)]
+		(loss pt rt)))
+
+(defn oco
+	"Runs an OCO game"
+	[]
+	(iterate 
+		()))
 
 (defn player-create 
 	[start loss]
